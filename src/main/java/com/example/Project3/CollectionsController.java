b51abe56ef9358a0e44eac985db0b86793ca9319
Rene Vega <renev2013@gmail.com>
2025-11-05 18:52:32 -0800

    added route to get users collection by id

--- src/main/java/com/example/Project3/CollectionsController.java
+++ src/main/java/com/example/Project3/CollectionsController.java
@@ -2,18 +2,29 @@ package com.example.Project3;
 
 import com.example.Project3.entities.Collections;
 import java.util.List;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/collections")
 public class CollectionsController {
   private final CollectionsRepo collectionsRepo;
+
   public CollectionsController(CollectionsRepo collectionsRepo) {
     this.collectionsRepo = collectionsRepo;
   }
 
-  @GetMapping ("/collections")
+//  @GetMapping ("/collections")
   List<Collections> getCollections(){
     return collectionsRepo.findAll();
+  }
+  @GetMapping("/{id}")
+  ResponseEntity<Collections> getCollection(@PathVariable long id) {
+    return collectionsRepo.findById(id)
+        .map(ResponseEntity::ok)
+        .orElse(ResponseEntity.notFound().build());
   }
 }
