5cff28aebeb2e2ecdd6ecb2a4d8227c9c921c4f0
Emily <emily.madsen03@gmail.com>
2025-11-05 17:00:11 -0800

    more controllers, more routes for poses

--- src/main/java/com/example/Project3/PosesController.java
+++ src/main/java/com/example/Project3/PosesController.java
@@ -4,21 +4,39 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Project3.entities.Poses;
 
 @RestController
+@RequestMapping("/poses")
 public class PosesController {
     private final PosesRepository posesRepo;
 
     public PosesController(PosesRepository posesRepo){
         this.posesRepo = posesRepo;
     }
-
-    @GetMapping("/poses")
+    // ok to get all poses 
+    // i want to change this to be /poses/all
+    @GetMapping("/all")
     public List<Poses> all(){
         return posesRepo.findAll();
     }
+
+    // style filtering 
+    // i want this route to be /poses/{style}
+    @GetMapping("/{style}") 
+    public List<Poses> byStyle(@PathVariable String style){
+        return posesRepo.findByStyle(style);
+    }
+
+
+    // difficulty 
+
+
+    // search by keyword in description 
+
+
     
 }
--- src/main/java/com/example/Project3/PosesRepository.java
+++ src/main/java/com/example/Project3/PosesRepository.java
@@ -1,9 +1,13 @@
 package com.example.Project3;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.Project3.entities.Poses;
 
 public interface PosesRepository extends JpaRepository<Poses,Integer > {
+
+    List<Poses> findByStyle(String style);
   
 }
