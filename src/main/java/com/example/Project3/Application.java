7556419676f30d46a4803f23a377fbe4e7cb8fbb
Rene Vega <renev2013@gmail.com>
2025-11-12 18:33:06 -0800

    fixed post and get item by id in collections controller

--- src/main/java/com/example/Project3/Application.java
+++ src/main/java/com/example/Project3/Application.java
@@ -8,8 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-//@SpringBootApplication
-@SpringBootApplication(scanBasePackages = "com.example.Project3")
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
